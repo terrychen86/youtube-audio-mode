dc52bce6e3e9b7dca14a0158f6fb3473d3e440b5
Terry Chen <yenhsuan.terry@gmail.com>
2020-02-14 00:46:50 -0800

    Add icons and dev manifest.json (#10)
    * Add icons
    
    * Add dev manifest.json
    
    * Remove default locale
    
    * Update description
    
    * Update permission

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,6 +2,7 @@
 
 const webpack = require('webpack');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const baseConfig = require('./webpack.base.config');
 
 const config = {
@@ -11,6 +12,13 @@ const config = {
       new OptimizeCSSAssetsPlugin()
     ],
   },
+  plugins: [
+    ...baseConfig.plugins,
+    new CopyPlugin([
+      { from: 'static', to: '' },
+      { from: 'config-prod', to: '' },
+    ]),
+  ]
 };
 
 module.exports = config;
